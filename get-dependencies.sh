04167a68625de82c4aed74b95586a94c08bac4bd
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-01-26 00:55:30 -0400

    use official deb as source

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,17 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm signal-desktop
+pacman -Syu --noconfirm \
+  libcups       \
+  libdrm        \
+  libnotify     \
+  libpulse      \
+  libxcb        \
+  libxcomposite \
+  libxdamage    \
+  libxext       \
+  libxfixes     \
+  libxrandr
 
 if [ "$ARCH" = 'x86_64' ]; then
 	sudo pacman -S --noconfirm libva-intel-driver
@@ -19,4 +29,27 @@ get-debloated-pkgs --add-common --prefer-nano intel-media-driver-mini
 # Comment this out if you need an AUR package
 #make-aur-package PACKAGENAME
 
-# If the application needs to be manually built that has to be done down here
+# get latest deb directly from upstream
+latest_ver=$(
+	wget https://updates.signal.org/desktop/apt/dists/xenial/main/binary-amd64/Packages.gz -O - \
+	| zcat | grep -v beta | awk -F'/' '/Filename: /{print $NF; exit}' | awk -F'_' '{print $(NF-1)}'
+)
+
+if [ -z "$latest_ver" ]; then
+	>&2 echo "Something went wrong getting latest version"
+	exit 1
+fi
+
+case "$ARCH" in
+	x86_64)  debarch=amd64;;
+	aarch64) debarch=arm64;;
+esac
+
+DEB_LINK=https://updates.signal.org/desktop/apt/pool/s/signal-desktop/signal-desktop_"$latest_ver"_"$debarch".deb
+wget "$DEB_LINK" -O /tmp/temp.deb
+ar x /tmp/temp.deb
+tar xvf ./data.tar.xz
+mkdir -p ./AppDir/bin
+mv -v ./opt/* ./AppDir/bin
+cp -v ./usr/share/icons/hicolor/256x256/apps/signal-desktop.png ./AppDir
+cp -v ./usr/share/icons/hicolor/256x256/apps/signal-desktop.png ./.DirIcon
