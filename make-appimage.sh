fe1a94b1298240c5d3b57beacba1115d7bcf562e
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-01-26 00:56:52 -0400

    simplify

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,18 +3,14 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q signal-desktop | awk '{print $2; exit}') # example command to get version of application here
-export ARCH VERSION
+export ARCH
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook:fix-namespaces.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=/usr/share/icons/hicolor/128x128/apps/signal-desktop.png
-export DESKTOP=/usr/share/applications/signal-desktop.desktop
 export OPTIMIZE_LAUNCH=1
 
 # Deploy dependencies
 mkdir -p ./AppDir/bin
-cp -r /usr/lib/signal-desktop/* ./AppDir/bin
 quick-sharun ./AppDir/bin/*
 
 # Additional changes can be done in between here
